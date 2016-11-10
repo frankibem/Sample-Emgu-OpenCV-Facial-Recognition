432096b339319c2ce2814e393606f31305bd7684
Frank Ibem <soulsolja@yahoo.com>
2016-11-09 21:53:23 -0600

    Add project files.

---
+++ RecognitionDemo/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+using swf = System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using Emgu.CV.Face;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace RecognitionDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string cascadePath = @"haarcascade_frontalface_default.xml";
+        private string trainedData = @"C:\Users\souls\Pictures\Demo\train.yaml";
+        private string labelPath = @"C:\Users\souls\Pictures\Demo\labels.csv";
+        private Mat original;
+
+        private CascadeClassifier detector;
+        private LBPHFaceRecognizer recognizer;
+
+        // Associates ids to names
+        //private Dictionary<int, string> names = new Dictionary<int, string>() { { 0, "Lauren" }, { 1, "Frank" }, { 2, "Michael" }, { 3, "Claire" }, { 4, "Josh" } };
+        private Dictionary<int, string> names;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Initialize detector and recognizer
+            detector = new CascadeClassifier(cascadePath);
+            recognizer = new LBPHFaceRecognizer();
+            recognizer.Load(trainedData);
+
+            // Load the labels
+            names = new Dictionary<int, string>();
+            var labels = System.IO.File.ReadAllLines(labelPath);
+            foreach(var line in labels)
+            {
+                var content = line.Split(',');
+                names.Add(int.Parse(content[0]), content[1]);
+            }
+        }
+
+        private void Select_Image(object sender, RoutedEventArgs e)
+        {
+            // Show File Picker
+            var fileDialog = new swf.OpenFileDialog();
+            fileDialog.RestoreDirectory = true;
+
+            var result = fileDialog.ShowDialog();
+            if(result != swf.DialogResult.OK)
+                return;
+
+            // Display selected image
+            original = new Mat(fileDialog.FileName, Emgu.CV.CvEnum.LoadImageType.Color);
+            imageBox.Source = ToBitmapSource(original);
+        }
+
+        private void Recognize_Face(object sender, RoutedEventArgs e)
+        {
+            // Convert to grayscale
+            var image = original.ToImage<Bgr, byte>();
+            var gray = image.Convert<Gray, byte>();
+
+            // Detect face(s)
+            var faceRects = detector.DetectMultiScale(gray, 1.4, 4);
+
+            // Recognize faces and label
+            var filtered = Filter(faceRects);
+            foreach(var faceRect in filtered)
+            {
+                var scaled = gray.GetSubRect(faceRect).Resize(100, 100, Emgu.CV.CvEnum.Inter.Area);
+                var result = recognizer.Predict(scaled);
+
+                // Ignore if above threshold
+                if(result.Distance >= 90)
+                    continue;
+
+                // Frame using rectangle
+                image.Draw(faceRect, new Bgr(System.Drawing.Color.Green), 2);
+
+                // Get label
+                string name;
+                if(!names.TryGetValue(result.Label, out name))
+                    name = "Unknown";
+
+                // Format label and accuracy
+                name = string.Format("{0} - {1:f2}", name, result.Distance);
+
+                // Draw text
+                int y = Math.Max(0, faceRect.Y - 10);
+                image.Draw(name,
+                    new System.Drawing.Point(faceRect.X, y),
+                    Emgu.CV.CvEnum.FontFace.HersheyTriplex, 0.6,
+                    new Bgr(System.Drawing.Color.Yellow));
+            }
+
+            imageBox.Source = ToBitmapSource(image);
+        }
+
+        // Removes rectangles that are solely contained in another
+        private List<System.Drawing.Rectangle> Filter(System.Drawing.Rectangle[] rects)
+        {
+            var result = new List<System.Drawing.Rectangle>();
+            for(int i = 0; i < rects.Length; i++)
+            {
+                bool insideOther = false;
+                for(int j = i + 1; j < rects.Length; j++)
+                {
+                    if(isInside(rects[i], rects[j]))
+                    {
+                        insideOther = true;
+                        break;
+                    }
+                }
+
+                if(insideOther)
+                    continue;
+                else
+                    result.Add(rects[i]);
+            }
+
+            return result;
+        }
+
+        // Returns true if the first rectangle is inside the second and false otherwise
+        private bool isInside(System.Drawing.Rectangle first, System.Drawing.Rectangle second)
+        {
+            return first.X >= second.X &&
+                first.Y >= second.Y &&
+                ((first.X + first.Width) <= (second.X + second.Width)) &&
+                ((first.Y + first.Height) <= (second.Y + second.Height));
+        }
+
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        public static BitmapSource ToBitmapSource(IImage image)
+        {
+            using(Bitmap source = image.Bitmap)
+            {
+                IntPtr ptr = source.GetHbitmap();
+                BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    ptr,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+
+                DeleteObject(ptr);
+                return bs;
+            }
+        }
+    }
+}
---
+++ Trainer/Program.cs
@@ -0,0 +1,145 @@
+﻿using Emgu.CV;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Trainer
+{
+    public class Program
+    {
+        private static string cascadePath = @"haarcascade_frontalface_default.xml";
+
+        static void ExitOnError()
+        {
+            Console.Error.WriteLine("Usage: prepare <inDir> <outDir>");
+            Console.Error.WriteLine("Usage: train <inDir> <outPath>");
+            Environment.Exit(-1);
+        }
+
+        static void Main(string[] args)
+        {
+            if(args.Length != 3)
+                ExitOnError();
+
+            switch(args[0])
+            {
+                case "prepare":
+                    PrepareTrainingData(args[1], args[2]);
+                    break;
+                case "train":
+                    TrainRecognizer(args[1], args[2]);
+                    break;
+                default:
+                    ExitOnError();
+                    break;
+            }
+            //Stitch();
+        }
+
+        // Extracts faces in the given input directory and saves them to the given output directory
+        static void PrepareTrainingData(string inputDir, string outputDir)
+        {
+            int imageNumber = 0;
+            CascadeClassifier detector = new CascadeClassifier(cascadePath);
+
+            foreach(var fileName in Directory.EnumerateFiles(inputDir))
+            {
+                // Load the image and detect faces in it
+                Mat img = CvInvoke.Imread(fileName, Emgu.CV.CvEnum.LoadImageType.Grayscale);
+                var faces = detector.DetectMultiScale(img);
+
+                // Resize and save the faces to the output directory
+                foreach(var faceRect in faces)
+                {
+                    var image = new Mat(img, faceRect).ToImage<Bgr, byte>();
+                    image = image.Resize(100, 100, Emgu.CV.CvEnum.Inter.Area);
+
+                    string path = Path.Combine(outputDir, imageNumber.ToString() + ".jpg");
+                    Console.WriteLine($"Writing {path}");
+
+                    image.Save(path);
+                    imageNumber++;
+                }
+            }
+
+            Console.WriteLine($"\nDetected {imageNumber} faces.");
+        }
+
+        // Trains a recognizer using the labeled images in inputDir and saves the result
+        // to outputPath
+        static void TrainRecognizer(string inputDir, string outputPath)
+        {
+            var imageFiles = Directory.EnumerateFiles(inputDir).ToList();
+            var images = new Image<Gray, byte>[imageFiles.Count];
+            var labels = new int[imageFiles.Count];
+
+            // Load each file and it's label
+            Console.WriteLine("Loading data...");
+            int i = 0;
+            foreach(var imageFile in imageFiles)
+            {
+                var label = Path.GetFileNameWithoutExtension(imageFile).Split('_')[0];
+                labels[i] = int.Parse(label);
+                images[i] = CvInvoke.Imread(imageFile, Emgu.CV.CvEnum.LoadImageType.Grayscale).ToImage<Gray, byte>();
+
+                i++;
+            }
+            Console.WriteLine("Loading complete.");
+
+            // Train the recognizer and save the result
+            Console.WriteLine("Training...");
+            LBPHFaceRecognizer recognizer = new LBPHFaceRecognizer();
+            recognizer.Train(images, labels);
+            Console.WriteLine("Training done. Saving results...");
+            recognizer.Save(outputPath);
+        }
+
+        static void Stitch()
+        {
+            string path = @"C:\Users\souls\Pictures\Demo\processed";
+            string stitchOut = @"C:\Users\souls\Pictures\Demo\stitched.jpg";
+            var files = Directory.EnumerateFiles(path).ToList();
+
+            List<Image<Gray, byte>> rows = new List<Image<Gray, byte>>();
+            Image<Gray, byte> cur = null;
+
+            // Stitch images horizontally into rows
+            Console.WriteLine($"Stitching {files.Count} images...");
+            for(int i = 0; i < files.Count; i++)
+            {
+                var image = new Image<Gray, byte>(files[i]);
+                if(cur == null)
+                    cur = image;
+                else
+                    cur = cur.ConcateHorizontal(image);   // Stitch horizontally
+
+                if((i + 1) % 6 == 0)
+                {
+                    rows.Add(cur);
+                    cur = null;
+                }
+            }
+            if(cur != null)
+                rows.Add(cur);
+
+            // Stitch rows vertically into grid
+            Console.WriteLine($"Stitching {rows.Count} rows into a grid");
+            if(rows.Count == 0)
+                return;
+
+            cur = rows[0];
+            for(int i = 1; i < rows.Count; i++)
+            {
+                cur = cur.ConcateVertical(rows[i]);
+            }
+
+            Console.WriteLine("Stitching done");
+            Console.WriteLine($"Writing to {stitchOut}");
+            cur.Save(stitchOut);
+            Console.WriteLine("Done.");
+        }
+    }
+}
